599650c04388e0b92721edc5ba9a3cc640869fc1
Skxawng1204 <124446450+Skxawng1204@users.noreply.github.com>
2024-11-03 20:56:30 +0700

    Create mapdata.js

---
+++ mapdata.js
@@ -0,0 +1,535 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    VN01: {
+      name: "Lai Chau"
+    },
+    VN02: {
+      name: "Lào Cai"
+    },
+    VN03: {
+      name: "Hà Giang"
+    },
+    VN04: {
+      name: "Cao Bằng"
+    },
+    VN05: {
+      name: "Son La"
+    },
+    VN06: {
+      name: "Yên Bái"
+    },
+    VN07: {
+      name: "Tuyên Quang"
+    },
+    VN09: {
+      name: "Lạng Sơn"
+    },
+    VN13: {
+      name: "Quảng Ninh"
+    },
+    VN14: {
+      name: "Hòa Bình"
+    },
+    VN18: {
+      name: "Ninh Bình"
+    },
+    VN20: {
+      name: "Thái Bình"
+    },
+    VN21: {
+      name: "Thanh Hóa"
+    },
+    VN22: {
+      name: "Nghệ An"
+    },
+    VN23: {
+      name: "Ha Tinh"
+    },
+    VN24: {
+      name: "Quảng Bình"
+    },
+    VN25: {
+      name: "Quảng Trị"
+    },
+    VN26: {
+      name: "Thừa Thiên - Huế"
+    },
+    VN27: {
+      name: "Quàng Nam"
+    },
+    VN28: {
+      name: "Kon Tum"
+    },
+    VN29: {
+      name: "Quảng Ngãi"
+    },
+    VN30: {
+      name: "Gia Lai"
+    },
+    VN31: {
+      name: "Bình Định"
+    },
+    VN32: {
+      name: "Phú Yên"
+    },
+    VN33: {
+      name: "Đắk Lắk"
+    },
+    VN34: {
+      name: "Khánh Hòa"
+    },
+    VN35: {
+      name: "Lâm Đồng"
+    },
+    VN36: {
+      name: "Ninh Thuận"
+    },
+    VN37: {
+      name: "Tây Ninh"
+    },
+    VN39: {
+      name: "Đông Nam Bộ"
+    },
+    VN40: {
+      name: "Bình Thuận"
+    },
+    VN41: {
+      name: "Long An"
+    },
+    VN43: {
+      name: "Bà Rịa - Vũng Tàu"
+    },
+    VN44: {
+      name: "An Giang"
+    },
+    VN45: {
+      name: "Ðong Tháp"
+    },
+    VN46: {
+      name: "Tiền Giang"
+    },
+    VN47: {
+      name: "Kiên Giang"
+    },
+    VN49: {
+      name: "Vĩnh Long"
+    },
+    VN50: {
+      name: "Bến Tre"
+    },
+    VN51: {
+      name: "Trà Vinh"
+    },
+    VN52: {
+      name: "Sóc Trăng"
+    },
+    VN53: {
+      name: "Đông Bắc"
+    },
+    VN54: {
+      name: "Bắc Giang"
+    },
+    VN55: {
+      name: "Bạc Liêu"
+    },
+    VN56: {
+      name: "Bắc Ninh"
+    },
+    VN57: {
+      name: "Bình Dương"
+    },
+    VN58: {
+      name: "Bình Phước"
+    },
+    VN59: {
+      name: "Cà Mau"
+    },
+    VN61: {
+      name: "Hải Dương"
+    },
+    VN63: {
+      name: "Hà Nam"
+    },
+    VN66: {
+      name: "Đồng Bằng Sông Hồng"
+    },
+    VN67: {
+      name: "Nam Định"
+    },
+    VN68: {
+      name: "Phú Thọ"
+    },
+    VN69: {
+      name: "Thái Nguyên"
+    },
+    VN70: {
+      name: "Vĩnh Phúc"
+    },
+    VN71: {
+      name: "Điện Biên"
+    },
+    VN72: {
+      name: "Đắk Nông"
+    },
+    VN73: {
+      name: "Hau Giang"
+    },
+    VNCT: {
+      name: "Can Tho"
+    },
+    VNDN: {
+      name: "Đà Nẵng"
+    },
+    VNHN: {
+      name: "Ha Noi"
+    },
+    VNHP: {
+      name: "Hải Phòng"
+    },
+    VNSG: {
+      name: "Thành phố Hồ Chí Minh",
+      description: "Chaotic place in Vietnam",
+      url: "https://en.wikipedia.org/wiki/Ho_Chi_Minh_City",
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/0/08/Ho_Chi_Minh_City_Skyline_at_Night.jpg"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Hanoi",
+      lat: "21.028167",
+      lng: "105.854152"
+    }
+  },
+  labels: {
+    VN01: {
+      name: "Lai Chau",
+      parent_id: "VN01"
+    },
+    VN02: {
+      name: "Lào Cai",
+      parent_id: "VN02"
+    },
+    VN03: {
+      name: "Hà Giang",
+      parent_id: "VN03"
+    },
+    VN04: {
+      name: "Cao Bằng",
+      parent_id: "VN04"
+    },
+    VN05: {
+      name: "Son La",
+      parent_id: "VN05"
+    },
+    VN06: {
+      name: "Yên Bái",
+      parent_id: "VN06"
+    },
+    VN07: {
+      name: "Tuyên Quang",
+      parent_id: "VN07"
+    },
+    VN09: {
+      name: "Lạng Sơn",
+      parent_id: "VN09"
+    },
+    VN13: {
+      name: "Quảng Ninh",
+      parent_id: "VN13"
+    },
+    VN14: {
+      name: "Hòa Bình",
+      parent_id: "VN14"
+    },
+    VN18: {
+      name: "Ninh Bình",
+      parent_id: "VN18"
+    },
+    VN20: {
+      name: "Thái Bình",
+      parent_id: "VN20"
+    },
+    VN21: {
+      name: "Thanh Hóa",
+      parent_id: "VN21"
+    },
+    VN22: {
+      name: "Nghệ An",
+      parent_id: "VN22"
+    },
+    VN23: {
+      name: "Ha Tinh",
+      parent_id: "VN23"
+    },
+    VN24: {
+      name: "Quảng Bình",
+      parent_id: "VN24"
+    },
+    VN25: {
+      name: "Quảng Trị",
+      parent_id: "VN25"
+    },
+    VN26: {
+      name: "Thừa Thiên - Huế",
+      parent_id: "VN26"
+    },
+    VN27: {
+      name: "Quàng Nam",
+      parent_id: "VN27"
+    },
+    VN28: {
+      name: "Kon Tum",
+      parent_id: "VN28"
+    },
+    VN29: {
+      name: "Quảng Ngãi",
+      parent_id: "VN29"
+    },
+    VN30: {
+      name: "Gia Lai",
+      parent_id: "VN30"
+    },
+    VN31: {
+      name: "Bình Định",
+      parent_id: "VN31"
+    },
+    VN32: {
+      name: "Phú Yên",
+      parent_id: "VN32"
+    },
+    VN33: {
+      name: "Đắk Lắk",
+      parent_id: "VN33"
+    },
+    VN34: {
+      name: "Khánh Hòa",
+      parent_id: "VN34"
+    },
+    VN35: {
+      name: "Lâm Đồng",
+      parent_id: "VN35"
+    },
+    VN36: {
+      name: "Ninh Thuận",
+      parent_id: "VN36"
+    },
+    VN37: {
+      name: "Tây Ninh",
+      parent_id: "VN37"
+    },
+    VN39: {
+      name: "Đông Nam Bộ",
+      parent_id: "VN39"
+    },
+    VN40: {
+      name: "Bình Thuận",
+      parent_id: "VN40"
+    },
+    VN41: {
+      name: "Long An",
+      parent_id: "VN41"
+    },
+    VN43: {
+      name: "Bà Rịa - Vũng Tàu",
+      parent_id: "VN43"
+    },
+    VN44: {
+      name: "An Giang",
+      parent_id: "VN44"
+    },
+    VN45: {
+      name: "Ðong Tháp",
+      parent_id: "VN45"
+    },
+    VN46: {
+      name: "Tiền Giang",
+      parent_id: "VN46"
+    },
+    VN47: {
+      name: "Kiên Giang",
+      parent_id: "VN47"
+    },
+    VN49: {
+      name: "Vĩnh Long",
+      parent_id: "VN49"
+    },
+    VN50: {
+      name: "Bến Tre",
+      parent_id: "VN50"
+    },
+    VN51: {
+      name: "Trà Vinh",
+      parent_id: "VN51"
+    },
+    VN52: {
+      name: "Sóc Trăng",
+      parent_id: "VN52"
+    },
+    VN53: {
+      name: "Đông Bắc",
+      parent_id: "VN53"
+    },
+    VN54: {
+      name: "Bắc Giang",
+      parent_id: "VN54"
+    },
+    VN55: {
+      name: "Bạc Liêu",
+      parent_id: "VN55"
+    },
+    VN56: {
+      name: "Bắc Ninh",
+      parent_id: "VN56"
+    },
+    VN57: {
+      name: "Bình Dương",
+      parent_id: "VN57"
+    },
+    VN58: {
+      name: "Bình Phước",
+      parent_id: "VN58"
+    },
+    VN59: {
+      name: "Cà Mau",
+      parent_id: "VN59"
+    },
+    VN61: {
+      name: "Hải Dương",
+      parent_id: "VN61"
+    },
+    VN63: {
+      name: "Hà Nam",
+      parent_id: "VN63"
+    },
+    VN66: {
+      name: "Đồng Bằng Sông Hồng",
+      parent_id: "VN66"
+    },
+    VN67: {
+      name: "Nam Định",
+      parent_id: "VN67"
+    },
+    VN68: {
+      name: "Phú Thọ",
+      parent_id: "VN68"
+    },
+    VN69: {
+      name: "Thái Nguyên",
+      parent_id: "VN69"
+    },
+    VN70: {
+      name: "Vĩnh Phúc",
+      parent_id: "VN70"
+    },
+    VN71: {
+      name: "Điện Biên",
+      parent_id: "VN71"
+    },
+    VN72: {
+      name: "Đắk Nông",
+      parent_id: "VN72"
+    },
+    VN73: {
+      name: "Hau Giang",
+      parent_id: "VN73"
+    },
+    VNCT: {
+      name: "Can Tho",
+      parent_id: "VNCT"
+    },
+    VNDN: {
+      name: "Đà Nẵng",
+      parent_id: "VNDN"
+    },
+    VNHN: {
+      name: "Ha Noi",
+      parent_id: "VNHN"
+    },
+    VNHP: {
+      name: "Hải Phòng",
+      parent_id: "VNHP"
+    },
+    VNSG: {
+      name: "Hồ Chí Minh city",
+      parent_id: "VNSG"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
